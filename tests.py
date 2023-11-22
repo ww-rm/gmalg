d7abfb27222abf9e6f011386133e738101c2ce2c
ww-rm <ww-rm@qq.com>
2023-11-22 23:20:21 +0800

    update tests

--- tests.py
+++ tests.py
@@ -1,6 +1,7 @@
+import sys
 import unittest
 
-from gmalg import SM3
+from gmalg import SM3, SM4
 
 
 class TestSM3(unittest.TestCase):
@@ -20,6 +21,30 @@ class TestSM3(unittest.TestCase):
         self.assertEqual(self.h.value, bytes.fromhex("66c7f0f462eeedd9d1f2d46bdc10e4e24167c4875cf2f7a2297da02b8f4ba8e0"))
         self.h.update(b"dabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcd")
         self.assertEqual(self.h.value, bytes.fromhex("debe9ff92275b8a138604889c18e5a4d6fdb70e5387e5765293dcba39c0c5732"))
+
+
+class TestSM4(unittest.TestCase):
+    def setUp(self) -> None:
+        self.c = SM4(bytes.fromhex("0123456789ABCDEFFEDCBA9876543210"))
+
+    def test_case1(self):
+        cipher = self.c.encrypt(bytes.fromhex("0123456789ABCDEFFEDCBA9876543210"))
+        self.assertEqual(cipher, bytes.fromhex("681edf34d206965e86b3e94f536e4246"))
+
+        plain = self.c.decrypt(cipher)
+        self.assertEqual(plain, bytes.fromhex("0123456789ABCDEFFEDCBA9876543210"))
+
+    @unittest.skip("SM4 1000000 times encrypt and decrypt.")
+    def test_case2(self):
+        cipher = self.c.encrypt(bytes.fromhex("0123456789ABCDEFFEDCBA9876543210"))
+        for _ in range(999999):
+            cipher = self.c.encrypt(cipher)
+        self.assertEqual(cipher, bytes.fromhex("595298c7c6fd271f0402f804c33d3f66"))
+
+        plain = self.c.decrypt(cipher)
+        for _ in range(999999):
+            plain = self.c.decrypt(plain)
+        self.assertEqual(plain, bytes.fromhex("0123456789ABCDEFFEDCBA9876543210"))
 
 
 if __name__ == "__main__":
