0f1b7aaf3c3fe3a516c91937eb89b5f2dd3e3f68
ww-rm <ww-rm@qq.com>
2023-11-22 23:07:23 +0800

    add core

--- gmalg/__init__.py
+++ gmalg/__init__.py
@@ -1,5 +1,11 @@
 # -*- coding: UTF-8 -*-
 
-from .sm3 import SM3
+from gmalg.core.sm3 import SM3
+from gmalg.core.sm4 import SM4
 
-__version__ = "0.1.0"
+__all__ = [
+    "SM3",
+    "SM4",
+]
+
+__version__ = "0.9.0"
