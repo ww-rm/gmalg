af90af554e5acf21689b5ea46a2bc69684be7240
ww-rm <ww-rm@qq.com>
2023-11-22 16:44:57 +0800

    update tests.py

--- tests.py
+++ tests.py
@@ -4,22 +4,22 @@ from gmalg import SM3
 
 
 class TestSM3(unittest.TestCase):
+    def setUp(self) -> None:
+        self.h = SM3()
+
     def test_case1(self):
-        h = SM3()
-        h.update(b"abc")
-        self.assertEqual(h.value, bytes.fromhex("66c7f0f462eeedd9d1f2d46bdc10e4e24167c4875cf2f7a2297da02b8f4ba8e0"))
+        self.h.update(b"abc")
+        self.assertEqual(self.h.value, bytes.fromhex("66c7f0f462eeedd9d1f2d46bdc10e4e24167c4875cf2f7a2297da02b8f4ba8e0"))
 
     def test_case2(self):
-        h = SM3()
-        h.update(b"abcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcd")
-        self.assertEqual(h.value, bytes.fromhex("debe9ff92275b8a138604889c18e5a4d6fdb70e5387e5765293dcba39c0c5732"))
+        self.h.update(b"abcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcd")
+        self.assertEqual(self.h.value, bytes.fromhex("debe9ff92275b8a138604889c18e5a4d6fdb70e5387e5765293dcba39c0c5732"))
 
     def test_update(self):
-        h = SM3()
-        h.update(b"abc")
-        self.assertEqual(h.value, bytes.fromhex("66c7f0f462eeedd9d1f2d46bdc10e4e24167c4875cf2f7a2297da02b8f4ba8e0"))
-        h.update(b"dabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcd")
-        self.assertEqual(h.value, bytes.fromhex("debe9ff92275b8a138604889c18e5a4d6fdb70e5387e5765293dcba39c0c5732"))
+        self.h.update(b"abc")
+        self.assertEqual(self.h.value, bytes.fromhex("66c7f0f462eeedd9d1f2d46bdc10e4e24167c4875cf2f7a2297da02b8f4ba8e0"))
+        self.h.update(b"dabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcd")
+        self.assertEqual(self.h.value, bytes.fromhex("debe9ff92275b8a138604889c18e5a4d6fdb70e5387e5765293dcba39c0c5732"))
 
 
 if __name__ == "__main__":
