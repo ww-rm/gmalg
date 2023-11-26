feaa3eebb419704730871fe9f4d842210741cbee
ww-rm <ww-rm@qq.com>
2023-11-26 17:46:19 +0800

    update sm2

--- gmalg/sm2.py
+++ gmalg/sm2.py
@@ -7,12 +7,14 @@ from .sm3 import SM3
 __all__ = ["SM2"]
 
 
-_p = bytes.fromhex("FFFFFFFE FFFFFFFF FFFFFFFF FFFFFFFF FFFFFFFF 00000000 FFFFFFFF FFFFFFFF")
-_a = bytes.fromhex("FFFFFFFE FFFFFFFF FFFFFFFF FFFFFFFF FFFFFFFF 00000000 FFFFFFFF FFFFFFFC")
-_b = bytes.fromhex("28E9FA9E 9D9F5E34 4D5A9E4B CF6509A7 F39789F5 15AB8F92 DDBCBD41 4D940E93")
-_n = bytes.fromhex("FFFFFFFE FFFFFFFF FFFFFFFF FFFFFFFF 7203DF6B 21C6052B 53BBF409 39D54123")
-_xG = bytes.fromhex("32C4AE2C 1F198119 5F990446 6A39C994 8FE30BBF F2660BE1 715A4589 334C74C7")
-_yG = bytes.fromhex("BC3736A2 F4F6779C 59BDCEE3 6B692153 D0A9877C C62A4740 02DF32E5 2139F0A0")
+_ecdlp = ECDLP(
+    0xFFFFFFFE_FFFFFFFF_FFFFFFFF_FFFFFFFF_FFFFFFFF_00000000_FFFFFFFF_FFFFFFFF,
+    0xFFFFFFFE_FFFFFFFF_FFFFFFFF_FFFFFFFF_FFFFFFFF_00000000_FFFFFFFF_FFFFFFFC,
+    0x28E9FA9E_9D9F5E34_4D5A9E4B_CF6509A7_F39789F5_15AB8F92_DDBCBD41_4D940E93,
+    0xFFFFFFFE_FFFFFFFF_FFFFFFFF_FFFFFFFF_7203DF6B_21C6052B_53BBF409_39D54123,
+    0x32C4AE2C_1F198119_5F990446_6A39C994_8FE30BBF_F2660BE1_715A4589_334C74C7,
+    0xBC3736A2_F4F6779C_59BDCEE3_6B692153_D0A9877C_C62A4740_02DF32E5_2139F0A0,
+)
 
 
 class SM2(EllipticCurveCipher):
@@ -30,11 +32,7 @@ class SM2(EllipticCurveCipher):
 
             rnd_fn ((int) -> int): random function used to generate k-bit random number, default to `secrets.randbits`
         """
-        super().__init__(
-            ECDLP(int.from_bytes(_p, "big"), int.from_bytes(_a, "big"), int.from_bytes(_b, "big"),
-                  int.from_bytes(_n, "big"), int.from_bytes(_xG, "big"), int.from_bytes(_yG, "big")),
-            SM3, rnd_fn or self._default_rnd_fn, d=d, xP=xP, yP=yP, id_=id_
-        )
+        super().__init__(_ecdlp, SM3, rnd_fn or self._default_rnd_fn, d=d, xP=xP, yP=yP, id_=id_)
 
     def _default_rnd_fn(self, k: int) -> int:
         return secrets.randbits(k)
