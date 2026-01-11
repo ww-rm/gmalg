c052f0be7efe078ad102d28d2b149e9df9979260
ww-rm <ww-rm@qq.com>
2026-01-11 15:29:47 +0800

    add auto import

--- gmalg/__init__.py
+++ gmalg/__init__.py
@@ -2,20 +2,24 @@ from . import errors
 from .base import KEYXCHG_MODE, PC_MODE
 from .sm2 import SM2
 from .sm3 import SM3
-from .sm4 import SM4
+from .sm4 import SM4, SM4Cipher
 from .sm9 import SM9, SM9KGC
+from .utils import PADDING_MODE, DataPadder
 from .zuc import ZUC
 
 __all__ = [
-    "errors",
-    "KEYXCHG_MODE",
-    "PC_MODE",
     "SM2",
     "SM3",
     "SM4",
+    "SM4Cipher",
     "SM9",
     "SM9KGC",
     "ZUC",
+    "KEYXCHG_MODE",
+    "PC_MODE",
+    "PADDING_MODE",
+    "DataPadder",
+    "errors",
 ]
 
 __version__ = "1.0.6"
